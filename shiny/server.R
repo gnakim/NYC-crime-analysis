54673ccb25cf0686fc28198315eda067ceca3f62
Geena Kim <gkim@college.harvard.edu>
2020-12-03 23:28:09 -0500

    added more to about

--- shiny/server.R
+++ shiny/server.R
@@ -1,8 +1,8 @@
 library(DT)
 library(shiny)
+library(rstanarm) # for stan_glm
 
-
-crime_data = readRDS("crime_data.rds")
+crime_data <- readRDS("crime_data.RDS")
 
 shinyServer(function(input, output){
     
@@ -20,7 +20,7 @@ shinyServer(function(input, output){
         # nice$nice_names[which(nice$ugly_names == "boro_nm")]
         
         crime_data %>%
-            select(susp_age_group, susp_race, susp_sex,law_cat_cd) %>%
+            select(susp_age_group, susp_race, susp_sex, law_cat_cd) %>%
             filter(susp_age_group != "UNKNOWN", susp_race != "UNKNOWN", susp_sex != "U") %>%
             filter(!(susp_age_group %in% c("-928", "-942", "-965", "1925", "2019"))) %>%
             group_by(.data[[input$selectedSus]]) %>%
@@ -49,19 +49,28 @@ shinyServer(function(input, output){
             theme_bw()
     })
     
-    output$victimPlot <- renderPlot({
+    output$placePlot <- renderPlot({
         crime_data %>%
-            select(vic_age_group, vic_race, vic_sex,law_cat_cd) %>%
-            drop_na() %>%
-            filter(vic_age_group != "UNKNOWN", vic_race != "UNKNOWN", vic_sex != "U") %>%
-            group_by(.data[[input$selectedVic]]) %>%
+            select(boro_nm, parks_nm, prem_typ_desc, law_cat_cd) %>%
+            drop_na(input$selectedPlace) %>%
+            group_by(.data[[input$selectedPlace]]) %>%
             summarise(count = n(), .groups = "drop", law_cat_cd) %>%
-            ggplot(aes(x = fct_reorder(.data[[input$selectedVic]], count), y = count, fill = law_cat_cd)) +
+            ggplot(aes(x = fct_reorder(.data[[input$selectedPlace]], count), y = count, fill = law_cat_cd)) +
             geom_col() +
-            labs(title = "Characteristics of Crime Victims",
-                 y = "Number of Crimes Committed Against' Specified Group",
-                 x = input$selectedVic) +
+            labs(title = "Places Crimes Took Place",
+                 y = "Number of Crimes Committed In Specified Place",
+                 x = input$selectedPlace) +
             theme_bw()
     })
+    
+    output$modelPlot <- renderPlot({
+        fitted_model <- crime_data %>%
+            select(law_cat_cd, susp)
+            stan_glm(formula = ,
+                     family = gaussian(),
+                     refresh = 0)
+    })
+    
+    
     
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -2,7 +2,7 @@ library(DT)
 library(shiny)
 library(shinydashboard)
 
-crime_data = readRDS("crime_data.rds")
+crime_data <- readRDS("crime_data.RDS")
 
 shinyUI(dashboardPage(
     dashboardHeader(title = "NYC Crime Report Analysis"),
@@ -10,17 +10,18 @@ shinyUI(dashboardPage(
         
         sidebarUserPanel("NYC Crime Analysis 2019-2020"),
         sidebarMenu(
-            menuItem("Model", tabName = "model", icon = icon("bookmark")),
-            menuItem("Data Comparisons", tabName = "data_comp", icon = icon("chart-bar")),
+            menuItem("Model", tabName = "data_comp", icon = icon("chart-bar")),
             menuItem("Data", tabName = "data", icon = icon("database")),
+            # menuItem("Model", tabName = "model", icon = icon("bookmark")),
             menuItem("About", tabName = "about", icon = icon("user"))
         )
-        
     ),
+    
     dashboardBody(
         tags$head(
             tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
         ),
+        
         tabItems(
             tabItem(tabName = "data_comp",
                     fluidRow(plotOutput("suspectPlot")),
@@ -31,16 +32,27 @@ shinyUI(dashboardPage(
                     fluidRow(plotOutput("victimPlot")),
                     fluidRow(selectizeInput(inputId = "selectedVic",
                                             label = "Select Item to Display",
-                                            choice = colnames(crime_data), # c("name" = ugly)
-                                            selected = "vic_race"))),
+                                            choice = c("vic_age_group", "vic_race", "vic_sex"), # c("name" = ugly)
+                                            selected = "vic_race")),
+                    fluidRow(plotOutput("placePlot")),
+                    fluidRow(selectizeInput(inputId = "selectedPlace",
+                                            label = "Select Item to Display",
+                                            choice = c("boro_nm", "parks_nm", "prem_typ_desc"), # c("name" = ugly)
+                                            selected = "boro_nm"))),
             tabItem(tabName = "data",
                     fluidRow(box(DT::dataTableOutput("table"), width = 12))),
+            tabItem(tabName = "model",
+                    fluidRow(p("STAN GLM MODEL HERE")),
+                    fluidRow(plotOutput("modelPlot"))),
             tabItem(tabName = "about",
                     fluidRow(h3("Project Background and Motivations"),
-                             p("This project analyzes the NYPD crime reports from 2019-2020. It allows us to see how activity has changed since the onset of the COVID-19 pandemic and what stayed the same."),
+                             p("This project analyzes the NYPD crime reports from 2019-2020. 
+                               I specifically analyze th race, sex, and age of people involved in these crime reports, the suspect and victim.
+                               There is also a graph that analyzes the location of where the crime is committed. You can toggle between the boroughs,
+                               parks, and buildings in NYC."),
                              h3("About Me"),
-                             p("Hi! I'm Geena and I'm a sophomore at Harvard University.I'm studying computer science and chemistry."),
-                             br(),
+                             p("Hi! I'm Geena and I'm a sophomore at Harvard University
+                               studying computer science and chemistry."),
                              p("You can reach me at gkim@college.harvard.edu.")))
         )
     )
